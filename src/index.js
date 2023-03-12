975611b564230bd455851a2a349db704ebfce565
horatiu84 <horatiul@yahoo.com>
2023-03-12 11:20:58 +0200

    creating navbar

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './App';
+
+const theme = createTheme({});
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+
+  </ThemeProvider>,
 );
